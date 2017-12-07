98b843b3db8659b210d110800e2b67b2cfcc8a35
Lopale505 <alopez167@mail.csuchico.edu>
2017-12-06 20:23:38 -0800

    8:22pm wed

---
+++ bitmapEditor.cpp
@@ -0,0 +1,29 @@
+#include "bitmapEditor.h"
+#include "bitmap.h"
+#include<string>
+#include<vector>
+
+using namespace std;
+
+vector<vector<Pixel> > bitmapEditor :: oldTimeyPhoto(void){
+  vector<vector<Pixel> > oldTimeyBmp;
+  oldTimeyBmp = 
+    //question: how do make oldTimeyBmp the image?
+
+}
+
+vector<vector<Pixel> > bitmapEditor :: brightness(void){
+
+}
+
+vector<vector<Pixel> > bitmapEditor :: warmth(void){
+
+}
+
+vector<vector<Pixel> > bitmapEditor :: saturation(void){
+
+}
+
+vector<vector<Pixel> > bitmapEditor :: border(void){
+
+}
