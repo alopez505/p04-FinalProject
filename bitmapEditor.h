a2dce20649fd22f2e61df93b0fb998c9b628c926
Lopale505 <alopez167@mail.csuchico.edu>
2017-12-06 20:22:50 -0800

    8:22pm wed

---
+++ bitmapEditor.h
@@ -0,0 +1,21 @@
+#ifndef _BITMAPEDIT_H_
+#define _BITMAPEDIT_H_
+#include<string>
+#include<vector>
+#include"bitmap.h"
+
+using namespace std;
+
+class bitmapEditor : public Bitmap
+{
+  private:
+
+  public:
+    vector<vector<Pixel> > oldTimeyPhoto (void);
+    vector<vector<Pixel> > brightness (void);
+    vector<vector<Pixel> > warmth (void);
+    vector<vector<Pixel> > saturation (void);
+    vector<vector<Pixel> > border (void);
+
+};
+#endif
