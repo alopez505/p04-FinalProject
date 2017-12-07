99a3a369784680cb1948ba191710b0b894abb311
Lopale505 <alopez167@mail.csuchico.edu>
2017-12-06 21:16:07 -0800

    9:15pm wed

--- bitmapEditor.cpp
+++ bitmapEditor.cpp
@@ -7,23 +7,36 @@ using namespace std;
 
 vector<vector<Pixel> > bitmapEditor :: oldTimeyPhoto(void){
   vector<vector<Pixel> > oldTimeyBmp;
-  oldTimeyBmp = 
-    //question: how do make oldTimeyBmp the image?
-
+  oldTimeyBmp = toPixelMatrix();
+  
+  return oldTimeyBmp;
 }
 
 vector<vector<Pixel> > bitmapEditor :: brightness(void){
+  vector<vector<Pixel> > brightBmp;
+  brightBmp = toPixelMatrix();
 
+  return brightBmp;
 }
 
 vector<vector<Pixel> > bitmapEditor :: warmth(void){
+  vector<vector<Pixel> > warmthBmp;
+  warmthBmp = toPixelMatrix();
 
+  return warmthBmp;
 }
 
 vector<vector<Pixel> > bitmapEditor :: saturation(void){
+  vector<vector<Pixel> > saturationBmp;
+  saturationBmp = toPixelMatrix();
+
+  return saturationBmp;
 
 }
 
 vector<vector<Pixel> > bitmapEditor :: border(void){
+  vector<vector<Pixel> > borderBmp;
+  borderBmp = toPixelMatrix();
 
+  return borderBmp;
 }
