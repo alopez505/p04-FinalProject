22c8b97ec3d983f0ee5d4b72efe97aa0482a7098
Lopale505 <alopez167@mail.csuchico.edu>
2017-12-08 12:05:45 -0800

    12:05 fri

--- bitmapEditor.cpp
+++ bitmapEditor.cpp
@@ -79,28 +79,98 @@ vector<vector<Pixel> > bitmapEditor :: brightness(void){
       brightBmp[row][col] = rgb;
     }
   }
-
   return brightBmp;
 }
 
 vector<vector<Pixel> > bitmapEditor :: warmth(void){
   vector<vector<Pixel> > warmthBmp;
+  Pixel rgb;
+  char warmthChoice;
   warmthBmp = toPixelMatrix();
-
+  cout << "Warmth allows you to add a red or blue hue to your photo. Type (R) for red hue, type (B) for blue hue" << endl;
+  cin >> warmthChoice;
+  if(warmthChoice != 'r' && warmthChoice != 'R' && warmthChoice != 'b' && warmthChoice != 'B')
+  {
+    do{
+      cout << warmthChoice << " is not not a valid responce, please enter (R) for red, or (B) for blue. " << endl;
+      cin >> warmthChoice;
+    }while(warmthChoice != 'r' && warmthChoice != 'R' && warmthChoice != 'b' && warmthChoice != 'B');
+  }
+  if(warmthChoice == 'r' || warmthChoice == 'R')
+  {
+    int amtRed;
+    cout << "How much red would you like to add? (0-100%)" << endl;
+    cin >> amtRed;
+    int redness = amtRed * 2.55;
+    for(int row = 0; row < warmthBmp.size(); row++)
+    {
+      for(int col = 0; col < warmthBmp[row].size(); col++)
+      {
+        rgb = warmthBmp[row][col];
+        rgb.red = rgb.red + redness;
+        if(rgb.red > 255)
+        {
+          rgb.red = 255;
+        }
+        warmthBmp[row][col] = rgb;
+      }
+    }
+  }
+  if(warmthChoice == 'b' || warmthChoice == 'B')
+  {
+    int amtBlue;
+    cout << "How much blue would you like to add? (0-100%)" << endl;
+    cin >> amtBlue;
+    int blueness = amtBlue * 2.55;
+    for(int row=0; row < warmthBmp.size(); row++)
+    {
+      for(int col = 0; col < warmthBmp[row].size(); col++)
+      {
+        rgb = warmthBmp[row][col];
+        rgb.blue = rgb.blue + blueness;
+        if(rgb.blue > 255)
+        {
+          rgb.blue = 255;
+        }
+        warmthBmp[row][col] = rgb;
+      }
+    }
+  }
   return warmthBmp;
 }
 
 vector<vector<Pixel> > bitmapEditor :: saturation(void){
   vector<vector<Pixel> > saturationBmp;
+  Pixel rgb;
+  double amtSat;
+  double sat;
   saturationBmp = toPixelMatrix();
-
+  cout << "How much saturation would you like to add? (0-100%)" << endl;
+  cin >> amtSat;
+  sat = amtSat / 100;
+  for(int row = 0; row < saturationBmp.size(); row++)
+  {
+    for(int col = 0; col < saturationBmp[row].size(); col++)
+    {
+      rgb = saturationBmp[row][col];
+      rgb.red = rgb.red + (rgb.red * sat);
+      if(rgb.red > 255)
+      {
+        rgb.red = 255;
+      }
+      rgb.green = rgb.green + (rgb.green * sat);
+      if(rgb.green > 255)
+      {
+        rgb.green = 255;
+      }
+      rgb.blue = rgb.blue + (rgb.blue * sat);
+      if(rgb.blue > 255)
+      {
+        rgb.blue = 255;
+      }
+      saturationBmp[row][col] = rgb;
+    }
+  }
   return saturationBmp;
-
 }
 
-vector<vector<Pixel> > bitmapEditor :: border(void){
-  vector<vector<Pixel> > borderBmp;
-  borderBmp = toPixelMatrix();
-
-  return borderBmp;
-}
